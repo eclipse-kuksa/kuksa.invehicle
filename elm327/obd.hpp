8f1bb0e036657f106d22b9af96e6662845add243
pratheek rai <pratheek.rai@de.bosch.com>
2018-07-23 14:31:36 +0200

    initial contribution signed-off-by:Pratheek Rai <pratheek.rai@de.bosch.com>

---
+++ elm327/obd.hpp
@@ -0,0 +1,20 @@
+/*
+ * ******************************************************************************
+ * Copyright (c) 2018 Robert Bosch GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/index.php
+ *
+ *  Contributors:
+ *      Robert Bosch GmbH - initial API and functionality
+ * *****************************************************************************
+ */
+#include<string>
+using namespace std;
+
+bool connectOBD(int timeout);
+string readMode1Data(string command);
+void closeConnection();
+int testCommands(string command, char* response);
