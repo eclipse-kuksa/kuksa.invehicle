8860ef554d206c220e6c0ea00ec51c3b0ca58b1c
alexioslekidis <36232325+alexioslekidis@users.noreply.github.com>
2018-08-29 14:58:02 +0200

    Update TimeIntervalsIDS.py Signed-off-by: Alexios Lekidis <alexios.lekidis@secmatters.com>
    Signed-off-by: Alexios Lekidis <alexios.lekidis@secmatters.com>

--- netIDS/TimeIntervalsIDS.py
+++ netIDS/TimeIntervalsIDS.py
@@ -35,29 +35,6 @@ can_data = [0,0,0,0,0,0,0,0,0,0]
 dicMsg=[]
 
 
-
-    #self.c.execute("CREATE TABLE {tn} ({nf} {ft})"\
-     #               .format(tn=table_name1, nf=alert, ft=field_type))
-    # Perform query and return JSON data
-    #query = conn.execute("select distinct DEPARTMENT from salaries")
-    #conn.commit()
-    #conn.close()
-
-def getdb():
-     DATABASE = 'my_first_db.sqlite'
-     db = getattr(g, '_database', None)
-     if db is None:
-         db = g._database = sqlite3.connect(DATABASE)
-         db.text_factory = str
-     return db
-
-
-        #with app.open_resource('my_first_db.sqlite', mode='r') as f:
-        #  db.cursor().executescript(f.read())
-      #db.commit()
-
-   #app.run()
-
 class BindClass:
     def __init__(self):
 
@@ -73,20 +50,10 @@ class BindClass:
         self.counter=0
         self.ta=0
 
-        #self.thread1 = myThread(1, "Thread-1", 1)
-        #self.thread1.daemon = True
-        #self.thread1.start()
-
-        #self.c.execute('CREATE TABLE {tn} ({nf} {ft})' \
-         #         .format(tn=table_name1, nf=new_field, ft=field_type))
-
 
     def buildingModel(self,pkt):
 
         total = 0
-        #self.ta = time.time()
-        #To calculate the total duration of the data capture.
-        #We will save the first and last timestamps and substract them to each other.
         minTimestamp = 0
         maxTimestamp = 0
 
@@ -101,9 +68,6 @@ class BindClass:
 
         #print(self.ta)
         new_packet = pkt.getlayer(Raw).load
-        #new_packet1=str(new_packet1)
-        #new_packet1=new_packet
-        #print (new_packet)
         packet_time = pkt.time
         can_id = bytes(new_packet[0])
         length = len(new_packet)
@@ -151,45 +115,18 @@ class BindClass:
         self.c.execute("INSERT INTO items (Network, AlertID) VALUES(?,?)", ('lab',self.a))
         self.conn.commit()
 
-
-
-        #self.c.execute("CREATE TABLE {tn} ({nf} {ft})"\
-         #               .format(tn=table_name1, nf=alert, ft=field_type))
-        # Perform query and return JSON data
-        #query = conn.execute("select distinct DEPARTMENT from salaries")
-        #conn.commit()
-        #conn.close()
-
     def postprocess2(self):
         self.conn.close()
 
     #Anomaly detection
     def anomalyDetection(self,pkt):
-        #print("### Testing phase ###\n")
 
         minTimestamp = 0
         maxTimestamp = 0
         counter=0
 
-        # for line in model:
-        #     #pieces = line.split(' ')
-        #     canid = model['can_id'][counter]
-        #     frameTime=model['ts'][counter]
-        #     frameLength=model['len'][counter]
-        #     counter=counter+1
-
-
-        #model.seek(0, 0)
-
         has_anomaly = False
         anomaly_counter = 0
-
-
-        #dicMsg = {}
-        #Format of the dictionary: {'CAN ID': [counter, last timestamp] }
-        #for canid in model_normal_behav:
-            #an alert will be raised every three consecutive anomalies
-        #    dicMsg[canid] = [0, 0]
 
         dos_threshold = 1 #number of packets seen with time interval < 0.2 ms to raise a DOS attack alert
         dos_counter = 0
@@ -199,14 +136,9 @@ class BindClass:
         packet_time = pkt.time
 
         can_hex = binascii.hexlify(can_id)
-        # for line in tsd:
-        #     msg = SFFMessage(line)
+      
         if can_hex in self.model_normal_behav:
-        #         #In the paper they do not discussed how to handle unforeseen IDs
-        #     dicMsg[msg.id][0] += 1
-        #     if dicMsg[msg.id][0] == 1: #if this is the first time we see this ID
-        #         dicMsg[msg.id][1] = msg.ts
-        #         continue
+
             if (((pkt.time - self.prevTimestamp[can_hex]) > 0) & (int(abs(pkt.time - self.prevTimestamp[can_hex]))<int(self.model_normal_behav[can_hex][0]))):
                  has_anomaly = True
                  anomaly_counter += 1
@@ -214,35 +146,12 @@ class BindClass:
                  severityNum=1
                  l2proto="CAN"
                  algorithmName="TimeIntervals"
-                 #can_string=int(can_id, 16)
                  description="CAN ID " + str(can_hex) + " outside the average time interval"
                  self.a=str("CEF:0|RTC-IDS|" + "|{"+str(severityNum)+"}|"+"{"+algorithmName+"}|{"+l2proto+"msg={"+description+"}")
                  #{l2proto}| msg={description} proto={l4Proto} app={l7Proto} smac={srcMac} dmac={dstMac} src={srcIp} dst={dstIp} spt={srcPort} dpt={dstPort} cs1={messageIdentifier} in={messageLength} TrendMicroDsPacketData={messagePayload}
-                 file.write(self.a)
-                 file.close()
-                 #print("Injection attack ! CAN ID", pkt.id)
                  self.postprocess()
-                 #self.postprocess(sa)
-
-            # if abs(pkt.time - self.prevTimestamp[can_id]) < 0.0002:
-            #      dos_counter += 1
-            #      if dos_counter == dos_threshold:
-            #          print("DOS attack detected")
-            #          dos_counter = 0
 
             self.prevTimestamp[can_hex] = packet_time
-
-
-
-        #
-        # print("\n### Summary ###\n")
-        # if not has_anomaly:
-        #     print("Analysis done: no anomaly has been found.")
-        # else:
-        #     if anomaly_counter==1:
-        #         print("One anomaly has been found !")
-        #     else:
-        #         print("{0:d} anomalies have been found !\n".format(anomaly_counter))
 
 
 def main():
@@ -257,13 +166,6 @@ def main():
        cla=BindClass()
 
 
-    #e = create_engine('sqlite:///salaries.db')
-
-    #thread1 = myThread(1, "Thread-1", 1)
-    #thread1.daemon = True
-    #thread1.start()
-
-
     isreal_time=True
     if args[2]:
         test = args[2]
@@ -271,40 +173,24 @@ def main():
         test = False
 
     if isreal_time==True:
+        
+        # training phase
         trainingtime=float(args[0])
-        #ta=time.time()
-
-
-        # learning_phase
-        #while ((tb-ta)<=trainingtime):
-
-            #capture=sniff(iface="enp0s31f6", filter="host 127.0.0.1 and port 1337")
+        
         print("### Training phase ###\n")
-        #while (time.time() - cla.ta > trainingtime):
+        
         capture = sniff(iface=interface, prn=cla.buildingModel, stop_filter = lambda x: time.time()-cla.ta >= trainingtime)
-        print("Came from here")
 
         cla.process()
 
-        #if ~capture:
-        #    break
-
-        # training phase
+        # testing phase
         if test:
 
-        # capture=sniff(iface="enp0s31f6", filter="host 127.0.0.1 and port 1337")
-        # learning_phase
-        #args[0]+++
-        # training phase
             print("### Testing phase ###\n")
-
 
             capture = sniff(iface=interface, prn= cla.anomalyDetection)
 
             cla.postProcess()
-            #cla.postProcess2()
-            #if ~capture:
-            #    break
 
 
     elif isreal_time==False:
@@ -326,17 +212,4 @@ def main():
             td.close()
             tsd.close()
 
-
-#def pkt_callback(pkt):
-
-
-#    can_data=[]
-
-#    new_packet=pkt.getlayer(Raw).load
-#    packet_time=pkt.time
-#    can_id=new_packet[0]
-#    len=new_packet[4]
-#    can_data[0]=new_packet[8]
-
-#    return
 main()
