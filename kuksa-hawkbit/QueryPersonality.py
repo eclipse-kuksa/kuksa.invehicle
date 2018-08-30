00a8c7fd7bb939813916122e075e9c75989919a4
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2018-08-30 10:48:44 +0200

    Refactor feedback into util module
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

--- kuksa-hawkbit/QueryPersonality.py
+++ kuksa-hawkbit/QueryPersonality.py
@@ -12,6 +12,7 @@
 #
 
 
+
 import requests
 import os.path
 import datetime
@@ -19,28 +20,19 @@ import json
 
 import DeployDocker
 import DeployCopyStart
+import HawkbitUtil
 
 destination="/tmp"
 
-def feedback(server, actionId, state, stepCurrent=5, stepTo=5, execution="closed", feedback="KUKSA" ): 	
-    reply= {}
-    reply['id'] = actionId
-    reply['time'] = datetime.datetime.utcnow().isoformat()
-    reply['status'] = {}
-    reply['status']['result'] = {}
-    progress= { "of" : stepTo, "cnt": stepCurrent }
-    reply['status']['result']['progress']=progress 
-    reply['status']['result']['finished']=state 
-    reply['status']['execution'] =  execution 
-    reply['status']['details'] = [ feedback ]
-    
-    
-    return json.dumps(reply)
 
 
 def queryHawkbit(server, personality, dst):
     global destination
     destination=dst
+
+    dd = {}
+    dd['personality']=personality
+    dd['server']=server 
 
     print("Checking assets for device "+str(personality['target']))
     headers = {'Authorization': 'TargetToken '+personality['securityToken'], 'Accept': 'application/hal+json' }
@@ -68,24 +60,27 @@ def queryHawkbit(server, personality, dst):
 
     response = r.json()
     #print(str(r.json()))
-    downloadChunks(server,personality['target'],response, personality)
+    
+    dd['chunks']=r.json()
+    downloadChunks(dd)
 
 
 #We only expect one file, but download all artifacts from all chunks,
 #Todo: check what all the Hawkbit parameters mean. Probably Hawkbit can tell us waht we should do and when....
-def downloadChunks(server,target,deployment_desc, personality):
+def downloadChunks(dd):
     global destination
 
-    if "deployment" not in deployment_desc:
+    if "deployment" not in dd['chunks']:
         print("No deployment. Aborting")
         return
 
-    deployment=deployment_desc['deployment']
+    deployment=dd['chunks']['deployment']
     if "chunks" not in deployment:
         print("No chunks. Aborting")
         return
 
-    actionId=deployment_desc['id']
+    actionId=dd['chunks']['id']
+    dd['actionId']=dd['chunks']['id'] #helper functions expect it in top level
     chunks=deployment['chunks']
 
     for chunk in chunks:
@@ -103,7 +98,7 @@ def downloadChunks(server,target,deployment_desc, personality):
 
 
         #create dir for target
-        targetdir=str(destination)+"/"+str(target)
+        targetdir=str(destination)+"/"+str(dd['personality']['target'])
         if not os.path.exists(targetdir):
             os.makedirs(targetdir)
 
@@ -111,7 +106,7 @@ def downloadChunks(server,target,deployment_desc, personality):
         dst = targetdir+"/"+fname
 
         print("Download "+str(url)+" to "+str(dst))
-        headers = {'Authorization': 'TargetToken '+personality['securityToken'], "Content-type" : "application/json" }
+        headers = {'Authorization': 'TargetToken '+dd['personality']['securityToken'], "Content-type" : "application/json" }
         # NOTE the stream=True parameter
         r = requests.get(str(url), stream=True, headers=headers)
         with open(dst, 'wb') as f:
@@ -127,15 +122,13 @@ def downloadChunks(server,target,deployment_desc, personality):
         elif (fname.startswith("SIMPLE_")):
             print("Carful analysis in extensive checks have revealed this to be a boring app. Starting anyway")
             DeployCopyStart.deploy(dst)
+        else:
+            print("Download only")
             
 			
 		#Give feedback. For now we just say everything is fine
-        data=feedback(server,actionId, "success")
+        #data=feedback(dd['server'],actionId, "success")
+        HawkbitUtil.feedback(dd, "success");
         #print("Will send "+str(data))
-        r = requests.post(server+"/DEFAULT/controller/v1/"+personality['target']+"/deploymentBase/"+actionId+"/feedback", headers=headers, data=data, )
-        if r.status_code != 200:
-            print("Error reporting state " + str(r.status_code) + " when querying Hawkbit. Aborting")
-            print("Response was "+str(r.json()))
-            return
-   
+       
         print("Done: "+str(r.status_code))
