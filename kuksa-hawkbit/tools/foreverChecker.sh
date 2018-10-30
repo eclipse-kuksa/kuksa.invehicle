1c152f0ed2cd0031cd807ead7ff5300a258bc60e
Pratheek Rai <pratheek.rai@de.bosch.com>
2018-10-30 13:37:01 +0100

    Forever checker updated for kuksa-hawkbit
    Signed-off-by: Pratheek Rai <pratheek.rai@de.bosch.com>

--- kuksa-hawkbit/tools/foreverChecker.sh
+++ kuksa-hawkbit/tools/foreverChecker.sh
@@ -1,24 +1,25 @@
-#!/bin/sh
-#
-# Copyright (c) 2018 Eclipse KUKSA project
-#
-# This program and the accompanying materials are made available under the
-# terms of the Eclipse Public License 2.0 which is available at
-# http://www.eclipse.org/legal/epl-2.0
-#
-# SPDX-License-Identifier: EPL-2.0
-# Forever checker for people so poor that they do not even have cron
-#
-# Contributors: Robert Bosch GmbH
-
-DELAY=20
-DIR="../"
-
-
-cd $DIR
-while true; 
-do 
-	echo "Checking...."
-	python3 ./CheckNewSoftware.py
-	sleep $DELAY
+#!/bin/sh                                                                                                                                                                         
+#                                                                                                                                                                                 
+# Copyright (c) 2018 Eclipse KUKSA project                                                                                                                                        
+#                                                                                                                                                                                 
+# This program and the accompanying materials are made available under the                                                                                                        
+# terms of the Eclipse Public License 2.0 which is available at                                                                                                                   
+# http://www.eclipse.org/legal/epl-2.0                                                                                                                                            
+#                                                                                                                                                                                 
+# SPDX-License-Identifier: EPL-2.0                                                                                                                                                
+# Forever checker for people so poor that they do not even have cron                                                                                                              
+#                                                                                                                                                                                 
+# Contributors: Robert Bosch GmbH                                                                                                                                                 
+                                                                                                                                                                                  
+DELAY=5                                                                                                                                                                           
+                                                                                                                                                                                  
+coproc pwd                                                                                                                                                                        
+DIR=$(cat <&"${COPROC[0]}")                                                                                                                                                       
+pwd                                                                                                                                                                               
+                                                                                                                                                                                  
+while true;                                                                                                                                                                       
+do                                                                                                                                                                                
+        echo "Checking...."                                                                                                                                                       
+        python3 $DIR/CheckNewSoftware.py                                                                                                                                          
+        sleep $DELAY                                                                                                                                                              
 done
