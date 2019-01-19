fb6e8268756c17530d72c5a74967c2fe1b66f3c8
Sebastian Schildt <sebastian.schildt@de.bosch.com>
2019-01-19 22:05:07 +0100

    Initial Docker support for this package
    Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

---
+++ datalogger-mqtt/docker/Readme.md
@@ -0,0 +1,35 @@
+# Docker
+
+Build Kuksa-compatible dockers from this app. The Dockerfile is supposed to 
+be run through the magic build.sh wrapper.
+
+To build for am64 do
+
+`./build.sh amd64`
+
+to build for a Pi do
+
+`/build.sh armhf`
+
+If you try playing with the docker file directly, note that the build context
+needs to be the toplevel of kuksa.invehicle
+
+# Running the container
+This app needs Hono credentials. It uses the following environment variables
+
+| Variable      | What                               | 
+| ------------- |------------------------------------| 
+| HONOIP        | hostname or IP of your Hono        |
+| HONOPORT      | port your Hono instance listens on |   
+| HONODEVICE    | Your device in Hono                |
+| HONOPW        | Password for Hono                  |
+| TOKEN         | Token (for Hono?)                  |
+
+
+Example commandline
+ `docker run  --env HONOIP='11.11.11.111' --env HONOPORT='1883' --env HONODEVICE='sensor1' --env HONOPW='hono-secret' --env TOKEN='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFeGFtcGxlIEpXVCIsImlzcyI6IkVjbGlwc2Uga3Vrc2EiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE2MDkzNzI4MDB9.lxYf-N0C66NgJiinrY_pgVpZOxgH0ppnbMsdKfNiQjbMqYTY_nEpXufmjquvBaxiYsRb_3ScXA__gmviWj_PQoU3apuMVxJjAY6xrwaXFw7g-daUi21yqLSfLptH2vqQzhA_Y1LU6u7SjeiEcGRZrgq_bHnjMM61EVDSnKGCClo' amd64/kuksa-datalogger-mqtt:7e7c437`
+ 
+
+# Todo: 
+Code not 100% ready for docker yet. Need to set VIS server adress in source (it is hardcoded to localhost, but needs to point to docker host, such as 172.17.0.1). This should probably be a command line option
+
---
+++ datalogger-mqtt/docker/build.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+VERSION=$(git log -1 --pretty=format:%h)
+NAME="kuksa-datalogger-mqtt"
+
+if [ -z "$VERSION" ]; then
+    echo "Failed to extract version string" 1>&2
+    exit 1
+fi
+
+EXPERIMENTAL=`docker version | grep Experimental | tail -n 1 | xargs`
+if [ "$EXPERIMENTAL" != "Experimental: true" ]; then
+    echo "Please enable the docker engine's experimental features" 1>&2
+    exit 1
+fi
+
+function build {
+    ARCH=$1
+
+    # generate Dockerfile.build
+    case ${ARCH} in
+    'amd64')
+        sed -e "s/arm64v8/amd64/g" Dockerfile > Dockerfile.build
+        sed -i -e "s/^.*qemu-aarch64-static.*$//g" Dockerfile.build
+        ;;
+    'arm64')
+        cp /usr/bin/qemu-aarch64-static ./
+        if [ "$?" != "0" ]; then
+            echo "Please install the qemu-user-static package" 1>&2
+            exit 1
+        fi
+        cp Dockerfile Dockerfile.build
+        ;;
+    'armhf')
+        cp /usr/bin/qemu-arm-static ./
+        if [ "$?" != "0" ]; then
+            echo "Please install the qemu-user-static package" 1>&2
+            exit 1
+        fi
+        sed -e "s/arm64v8/armhf/g" Dockerfile > Dockerfile.build
+        sed -i -e "s/qemu-aarch64-static/qemu-arm-static/g" Dockerfile.build
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH" 1>&2
+        exit 1
+        ;;
+    esac
+
+    # build image
+    cd ../../
+    docker build --platform linux/$ARCH -f ./datalogger-mqtt/docker/Dockerfile.build -t ${ARCH}/${NAME}:${VERSION} .
+	cd datalogger-mqtt/docker
+
+    # cleanup
+    rm -f Dockerfile.build
+    rm -f qemu-aarch64-static
+    rm -f qemu-arm-static
+}
+
+ARCHS="$@"
+if [ -z "$ARCHS" ]; then
+    ARCHS="amd64 arm64 armhf"
+fi
+
+for ARCH in $ARCHS; do
+    build $ARCH
+done
