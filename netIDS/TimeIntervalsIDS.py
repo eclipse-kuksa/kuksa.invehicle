4854761c6c2b8dde9ea4b09c849561a4adaaf877
alekidis <alexios.lekidis@secmatters.com>
2019-02-28 15:41:42 +0100

    Update TimeIntervalsIDS.py

--- netIDS/TimeIntervalsIDS.py
+++ netIDS/TimeIntervalsIDS.py
@@ -19,8 +19,8 @@ from pathlib import Path
 import sqlite3
 from rawSniffer import sniff
 import sys
+import binascii
 from importlib import reload
-from bitstring import BitStream
 
 if sys.version[0] == '2':
     reload(sys)
@@ -57,8 +57,8 @@ class BindClass:
         new_packet = pkt.data
         packet_time = pkt.time
         can_packet = bytes(new_packet)
-        stream=BitStream(can_packet)
-        can_string=stream.read(8).hex
+        can_hex = binascii.hexlify(can_packet).decode('utf8')
+        can_string = can_hex[0]+can_hex[1]
         length = len(new_packet)
 
         msg_id.append(can_string)
@@ -67,7 +67,7 @@ class BindClass:
         self.dicMsg['ts'].append(packet_time)
         self.dicMsg['len'].append(length)
 
-        self.prevTimestamp.update({msg_id[0]: 0})
+        self.prevTimestamp.update({msg_id[0]: packet_time})
 
         if not msg_id in self.unique_ids:
             self.unique_ids.append(msg_id)
@@ -109,14 +109,13 @@ class BindClass:
         anomaly_counter = 0
 
         can_packet = bytes(new_packet)
-        stream=BitStream(can_packet)
-        can_string=stream.read(8).hex
-
+        can_hex = binascii.hexlify(can_packet).decode('utf8')
+        can_string = can_hex[0]+can_hex[1]
+        
         if can_string in self.model_normal_behav:
 
-            if (((pkt.time - self.prevTimestamp[can_string]) > 0) & (int(abs(pkt.time - self.prevTimestamp[can_string]))<int(self.model_normal_behav[can_string][0]))):
+            if (((pkt.time - self.prevTimestamp[can_string]) > 0) & (int(abs(pkt.time - self.prevTimestamp[can_string]))>int(self.model_normal_behav[can_string][0]))):
                  anomaly_counter += 1
-                 file = open("filename", 'a')
                  severityNum=1
                  l2proto="CAN"
                  algorithmName="TimeIntervals"
