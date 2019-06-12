0ee5ff18534c61ced8ceef868286d2c7ec3afc18
Pratheek Rai <pratheek.rai@de.bosch.com>
2019-06-12 13:14:23 +0200

    Kuksa dongle changes
    Signed-off-by: Pratheek Rai <pratheek.rai@de.bosch.com>

--- elm327-visdatafeeder/start.sh
+++ elm327-visdatafeeder/start.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-sleep 10s
+sleep 0.5s
 
-source bt_setup.sh
 echo "Starting ELM 327 app"
 
 
-SERIAL='/dev/rfcomm0'
+SERIAL='/dev/ttyAMA0'
 TOKEN='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFeGFtcGxlIEpXVCIsImlzcyI6IkVjbGlwc2Uga3Vrc2EiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE2MDkzNzI4MDB9.lxYf-N0C66NgJiinrY_pgVpZOxgH0ppnbMsdKfNiQjbMqYTY_nEpXufmjquvBaxiYsRb_3ScXA__gmviWj_PQoU3apuMVxJjAY6xrwaXFw7g-daUi21yqLSfLptH2vqQzhA_Y1LU6u7SjeiEcGRZrgq_bHnjMM61EVDSnKGCClo'
 
 
-./elm327-visdatafeeder $SERIAL $TOKEN 150000 150000
+./elm327-visdatafeeder $SERIAL $TOKEN 50000 3000000
 
 exit_code=$?
 
