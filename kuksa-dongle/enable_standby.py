f6ab7a918f3fdddc1dc9f5d489504be8a4069be0
Pratheek Rai <pratheek.rai@de.bosch.com>
2019-06-12 08:59:41 +0200

    Updated header data and removed python3 dependency
    Signed-off-by: Pratheek Rai <pratheek.rai@de.bosch.com>

--- kuksa-dongle/enable_standby.py
+++ kuksa-dongle/enable_standby.py
@@ -1,4 +1,12 @@
-#!/usr/bin/python3
+# Copyright (c) 2019 Eclipse KUKSA project
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors: Robert Bosch GmbH
 import wiringpi
 import os, time
 
