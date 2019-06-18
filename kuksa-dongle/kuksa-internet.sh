8469f5bfafb4a122a0894b090e6d05d523fa967a
Pratheek Rai <pratheek.rai@de.bosch.com>
2019-06-18 16:43:17 +0200

    bug fixes for internet and standby
    Signed-off-by: Pratheek Rai <pratheek.rai@de.bosch.com>

--- kuksa-dongle/kuksa-internet.sh
+++ kuksa-dongle/kuksa-internet.sh
@@ -4,51 +4,72 @@ PROVIDER="web.vodafone.de"
 NAME="vodafone"
 PASSWORD="vodafone"
 
-sleep 20s
+sleep 10s
+
+# Temp fix to reset USB hub
+echo 0 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio0/direction
+echo 0 > /sys/class/gpio/gpio0/value
+sleep 1s
+echo 1 > /sys/class/gpio/gpio0/value
+
+sleep 5s
+
 
 cd /usr/lib/ofono/test
-
 ./enable-modem
-sleep 1s
-./online-modem
+if [[ "$?" != "0" ]];
+    then
+       echo "Some exception occured so need to restart ofono"
+       systemctl restart ofono
+fi
+
 sleep 1s
 ./register-auto
 sleep 1s
 ./create-internet-context $PROVIDER $NAME $PASSWORD
 sleep 1s
 ./activate-context
-sleep 5s
+sleep 3s
 ./process-context-settings
+sleep 1s
+./online-modem
 
 sleep 30s
 
 echo "Enabled modem with context. Now entering infinite loop for checking internet"
 
 x=1
+count=0
 while [ $x -le 2 ]
 do
    echo "Checking internet ..."
-   if ping -q -c 1 -W 1 -w 5 8.8.8.8 >/dev/null; then
+   if ping -q -c 1 -W 3 -w 3 8.8.8.8 >/dev/null; then
     echo "IPv4 is up"
   else
     echo "IPv4 is down so resetting context and re-enabling modem"
-    echo "Restarting ofono service"
-    systemctl restart ofono
-    sleep 5s
     cd /usr/lib/ofono/test
     ./enable-modem
+    if [[ "$?" != "0" ]];
+       then
+         echo "Some exception occured so need to restart ofono"
+         systemctl restart ofono
+    fi
     sleep 1s
-    ./online-modem
+    ./deactivate-context
     sleep 1s
     ./register-auto
     sleep 1s
     ./create-internet-context $PROVIDER $NAME $PASSWORD
     sleep 1s
     ./activate-context
-    sleep 5s
+    sleep 3s
     ./process-context-settings
-
+    sleep 1s
+    ./online-modem
+    count=$((count + 1))
+    echo $count
   fi
+
 sleep 15s
 done
-
--- kuksa-dongle/kuksa-standby.sh
+++ kuksa-dongle/kuksa-standby.sh
@@ -7,17 +7,25 @@ echo 0 > /sys/class/gpio/gpio7/value
 echo 9 > /sys/class/gpio/export
 echo in > /sys/class/gpio/gpio9/direction
 
+
 while true
 do
 
 res=$(cat /sys/class/gpio/gpio9/value)
 if [ $res -eq 1 ]
 then
-
-halt
-
+echo "pin9 is high wait for 0.5 more seconds and if it is still high board will be shut down."
+sleep 0.5s
+res=$(cat /sys/class/gpio/gpio9/value)
+if [ $res -eq 1 ] 
+then
+#echo "Waited 0.5 seconds and the pin9 is still high so shutting down"
+#halt
+# FIX this- At the moment pi is not shut down but simply the power is removed.
+echo 1 > /sys/class/gpio/gpio7/value
 fi
-sleep 1s
+fi
+sleep 2s
 
 done
 
